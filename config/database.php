ef72972e01bd53867b85d6491abb14ecdbd3278c
SistemasMT <SistemasMT>
2017-03-23 17:12:56 -0600

    base de datos cleardb

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_1f5f2a66ee7f6e9'),
+            'username' => env('DB_USERNAME', 'b5ec7a7c1b1110'),
+            'password' => env('DB_PASSWORD', '8214aebb'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
